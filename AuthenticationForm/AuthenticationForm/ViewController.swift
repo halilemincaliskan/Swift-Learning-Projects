1f2a5e56b50c49c63514392eecd3a07978c6f08e
halilemincaliskan <halileminclskn@gmail.com>
2022-02-05 20:53:32 +0300

    Authentication Form Project Added
    I tried UIScrollView and UIView in this project.

---
+++ AuthenticationForm/AuthenticationForm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthenticationForm
+//
+//  Created by Halil on 4.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
